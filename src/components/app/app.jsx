2029fcb38ec611548272f30c6681f9786aba7fe5
Konstantin <rim1sta@MacBook-Pro-Konstantin.local>
2020-10-09 16:53:39 +0300

    add new components, add react-router-dom

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import DevArtist from "../dev-artist/dev-artist";
+import DevGenre from "../dev-genre/dev-genre";
+import Result from "../result/result";
+import Login from "../login/login";
+import GameOver from "../game-over/game-over";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <DevArtist/>
+        </Route>
+        <Route exact path="/dev-genre">
+          <DevGenre/>
+        </Route>
+        <Route exact path="/result">
+          <Result/>
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/game-over">
+          <GameOver/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
